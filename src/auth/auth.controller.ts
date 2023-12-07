634c49062567b063d4c8c6c08df9be6a31034075
Eldinho2 <viniciusfod@gmail.com>
2023-12-07 00:56:29 -0300

    comit

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,16 +3,16 @@ import { AuthService } from './auth.service';
 import { AuthRegisterDto } from './dto/authRegister.dto';
 import { AuthLoginDto } from './dto/authLogin.dto';
 
-@Controller('auth')
+@Controller('auth/signin/callback')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post('signup')
+  @Post('login')
   async signup(@Body() dto: AuthRegisterDto, @Response() res) {
     return this.authService.signup(dto, res);
   }
 
-  @Post('signin')
+  @Post('register')
   async signin(@Body() dto: AuthLoginDto, @Response() res) {
     return this.authService.signin(dto, res);
   }
